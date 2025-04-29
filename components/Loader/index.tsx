5b1563117cf0f26078f50ed3ea699ec63d58313a
monier <jercol.bs@gmail.com>
2025-04-29 15:49:59 -0700

    create: create the Loader component

---
+++ components/Loader/index.tsx
@@ -0,0 +1,25 @@
+import { CSSProperties } from "react";
+import { ClipLoader } from "react-spinners";
+
+const override: CSSProperties = {
+  borderColor: "rgb(7, 160, 129)",
+  border: "9px solid rgb(7, 160, 129)",
+};
+
+function Loader() {
+  return (
+    <div className="flex items-center justify-center h-screen w-screen">
+      <div>
+        <ClipLoader
+          loading={true}
+          cssOverride={override}
+          size={100}
+          aria-label="Loading Spinner"
+          data-testid="loader"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Loader;
