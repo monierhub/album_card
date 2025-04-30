efdbc3ae8c8ba2519031f00c505de5d3084fb4d0
monier <jercol.bs@gmail.com>
2025-04-30 12:49:53 -0700

    update: add the new image protocol

--- next.config.ts
+++ next.config.ts
@@ -20,7 +20,15 @@ const nextConfig: NextConfig = {
         port: "",
         pathname: "/**", // Allow any path on picsum.photos
       },
+      {
+        protocol: "https",
+        hostname: "i.ibb.co",
+        port: "",
+        pathname: "/**",
+      },
     ],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
 };
 
